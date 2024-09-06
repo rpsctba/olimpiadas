22a9b4879e15f4e0ec87942db8cab625d31ad4f3
rpsctba <118065671+rpsctba@users.noreply.github.com>
2024-09-06 11:53:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, conhecida carinhosamente como Fadinha, é um fenômeno do skate mundial. Aos poucos anos de idade, já conquistou o coração dos brasileiros e de amantes do esporte ao redor do globo. Vice-campeã olímpica nos Jogos de Tóquio e medalhista de bronze em Paris, Rayssa é a mais jovem medalhista olímpica brasileira da história, um feito inédito no esporte. Sua habilidade, carisma e determinação a transformaram em um verdadeiro ícone da juventude, inspirando milhares de jovens a seguirem seus sonhos e a praticarem esportes. Com seu estilo único e suas manobras desafiadoras, Rayssa Leal é a prova de que não há limites para o talento e a paixão quando se acredita em si mesmo.", 
+        link: "https://en.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza, a gigante do tatame, vem se destacando como uma das maiores promessas do judô brasileiro. Com sua força, técnica e determinação, a judoca conquistou o país ao garantir a primeira medalha de ouro para o Brasil nos Jogos Olímpicos de Paris 2024. Sua trajetória vitoriosa, marcada por títulos nacionais e internacionais, a tornou um símbolo de superação e inspiração para muitos jovens atletas. Aos poucos anos, Beatriz já coleciona diversas medalhas em competições de alto nível, consolidando seu lugar entre as melhores judocas do mundo. Com sua simpatia e carisma, a atleta transcende os limites do esporte, tornando-se um verdadeiro ícone para o Brasil.", 
+        link: "https://en.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade, a joia da ginástica brasileira, conquistou o coração dos brasileiros com sua habilidade, determinação e carisma. Bicampeã olímpica, ela é a maior medalhista olímpica do Brasil, acumulando diversas medalhas em competições nacionais e internacionais. Sua trajetória inspiradora, marcada por superação e dedicação, a tornou um símbolo do esporte brasileiro. Com sua beleza e simpatia, Rebeca transcende as quadras, tornando-se um verdadeiro ícone da juventude e um exemplo para as futuras gerações de atletas.", 
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
+
